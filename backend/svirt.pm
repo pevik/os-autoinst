pevik/os-autoinst

commit f7f66251a106ae0e93b61c6651055237fad9cc63

svirt backend: Add serial console for SUT

backend/svirt.pm
@@ -194,7 +194,27 @@ sub load_snapshot {
     return $rsp;
 }
 
-# Open another ssh connection to grab the serial console.
+sub read_credentials_from_virsh_variables {
+    my ($self) = @_;
+
+    my ($hostname, $username, $password);
+    if (check_var('VIRSH_VMM_FAMILY', 'hyperv')) {
+        $hostname = get_required_var('VIRSH_GUEST');
+        $password = get_var('VIRSH_GUEST_PASSWORD');
+    }
+    else {
+        $hostname = get_required_var('VIRSH_HOSTNAME');
+        $username = get_var('VIRSH_USERNAME');
+        $password = get_var('VIRSH_PASSWORD');
+    }
+    return {
+        hostname => $hostname,
+        username => ($username // 'root'),
+        password => $password,
+    };
+}
+
+# opens another SSH connection to grab the serial console for the serial log
 sub start_serial_grab {
     my ($self, $name) = @_;
 
@@ -209,7 +229,8 @@ sub start_serial_grab {
         $hostname = get_required_var('VIRSH_HOSTNAME');
         $password = get_var('VIRSH_PASSWORD');
     }
-    my $chan = $self->start_ssh_serial(hostname => $hostname, password => $password, username => 'root');
+    my $credentials = $self->read_credentials_from_virsh_variables;
+    my $chan        = $self->start_ssh_serial(%$credentials);
     if (check_var('VIRSH_VMM_FAMILY', 'vmware')) {
         # libvirt esx driver does not support `virsh console', so
         # we have to connect to VM's serial port via TCP which is
@@ -225,6 +246,35 @@ sub start_serial_grab {
     else {
         $chan->exec('virsh console ' . $name);
     }
+}
+
+# opens another SSH connection to grab the serial console with the specified port
+sub open_serial_console_via_ssh {
+    my ($self, $name, $port) = @_;
+
+    bmwqemu::diag("Starting SSH connection to connect to libvirt domain $name via serial port $port");
+    my $credentials = $self->read_credentials_from_virsh_variables;
+    my $ssh         = $self->new_ssh_connection(%$credentials);
+    my $chan        = $ssh->channel();
+    die 'No channel found' unless $chan;
+    $chan->blocking(0);
+    $chan->pty('vt100', {echo => 1});
+    $chan->pty_size(1024, 24);
+    $chan->shell();
+    print($chan "PS1='# '\n");
+
+    # note: see comments in start_serial_grab for the special handling of vmware/hyperv
+    if (check_var('VIRSH_VMM_FAMILY', 'vmware')) {
+        $chan->exec('nc ' . get_var('VMWARE_SERVER') . ' ' . $port);
+    }
+    elsif (check_var('VIRSH_VMM_FAMILY', 'hyperv')) {
+        $chan->exec('nc ' . get_var('HYPERV_SERVER') . ' ' . $port);
+    }
+    else {
+        $chan->exec("virsh console \"$name\" \"serial$port\"");
+    }
+
+    return ($ssh, $chan);
 }
 
 sub check_socket {

consoles/sshVirtshSUT.pm
@@ -1,0 +1,65 @@
+# Copyright © 2018 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+
+package consoles::sshVirtshSUT;
+use base 'consoles::console';
+use strict;
+use warnings;
+use testapi 'get_var';
+use consoles::virtio_screen;
+
+sub new {
+    my ($class, $testapi_console, $args) = @_;
+
+    my $self = $class->SUPER::new($testapi_console, $args);
+    $self->{libvirt_domain} = $args->{libvirt_domain} // 'openQA-SUT-1';
+    $self->{serial_port_no} = $args->{serial_port_no} // 1;
+    return $self;
+}
+
+sub screen {
+    my ($self) = @_;
+    return $self->{screen};
+}
+
+sub disable {
+    my ($self) = @_;
+
+    if (my $shell = $self->{shell}) {
+        $shell->close();
+    }
+    if (my $ssh = $self->{ssh}) {
+        $ssh->disconnect;
+        $self->{ssh} = $self->{chan} = $self->{screen} = undef;
+    }
+    return;
+}
+
+sub activate {
+    my ($self) = @_;
+
+    my $backend = $self->{backend};
+    my ($ssh, $chan) = $backend->open_serial_console_via_ssh($self->{libvirt_domain}, $self->{serial_port_no});
+
+    $self->{ssh} = $ssh;
+    $self->{screen} = consoles::virtio_screen->new($chan, $ssh->sock);
+    return;
+}
+
+sub is_serial_terminal {
+    return 1;
+}
+
+1;

consoles/virtio_screen.pm
@@ -23,9 +23,10 @@ use Carp 'croak';
 our $VERSION;
 
 sub new {
-    my ($class, $socket_fd) = @_;
+    my ($class, $socket_fd, $select_fd) = @_;
     my $self = bless {class => $class}, $class;
     $self->{socket_fd}    = $socket_fd;
+    $self->{select_fd}    = $select_fd;
     $self->{carry_buffer} = '';
     return $self;
 }
@@ -182,6 +183,7 @@ and { matched => 0, string => 'text from the terminal' } on failure.
 sub read_until {
     my ($self, $pattern, $timeout) = @_[0 .. 2];
     my $fd       = $self->{socket_fd};
+    my $sel_fd   = $self->{select_fd} || $fd;
     my %nargs    = @_[3 .. $#_];
     my $buflen   = $nargs{buffer_size} || 4096;
     my $overflow = $nargs{record_output} ? '' : undef;
@@ -197,7 +199,7 @@ sub read_until {
     bmwqemu::log_call(%nargs);
 
     my $rin = '';
-    vec($rin, fileno($fd), 1) = 1;
+    vec($rin, fileno($sel_fd), 1) = 1;
 
   READ: while (1) {
         $loops++;

distribution.pm
@@ -51,16 +51,17 @@ sub add_console {
     my ($self, $testapi_console, $backend_console, $backend_args) = @_;
 
     my %class_names = (
-        'tty-console'     => 'ttyConsole',
-        'ssh-xterm'       => 'sshXtermVt',
-        'ssh-virtsh'      => 'sshVirtsh',
-        'vnc-base'        => 'vnc_base',
-        'local-Xvnc'      => 'localXvnc',
-        'ssh-iucvconn'    => 'sshIucvconn',
-        'virtio-terminal' => 'virtio_terminal',
-        'amt-sol'         => 'amtSol',
-        'ipmi-sol'        => 'ipmiSol',
-        'ipmi-xterm'      => 'sshXtermIPMI',
+        'tty-console'       => 'ttyConsole',
+        'ssh-xterm'         => 'sshXtermVt',
+        'ssh-virtsh'        => 'sshVirtsh',
+        'ssh-virtsh-serial' => 'sshVirtshSUT',
+        'vnc-base'          => 'vnc_base',
+        'local-Xvnc'        => 'localXvnc',
+        'ssh-iucvconn'      => 'sshIucvconn',
+        'virtio-terminal'   => 'virtio_terminal',
+        'amt-sol'           => 'amtSol',
+        'ipmi-sol'          => 'ipmiSol',
+        'ipmi-xterm'        => 'sshXtermIPMI',
     );
     my $required_type = $class_names{$backend_console} || $backend_console;
     my $location      = "consoles/$required_type.pm";

commit 065c0c7bac69b613841ff1f3ed006547b352483e

svirt: Remove unused variables from start_serial_grab()
Fixes: f7f66251 ("svirt backend: Add serial console for SUT")

backend/svirt.pm
@@ -232,17 +232,6 @@ sub read_credentials_from_virsh_variables {
 sub start_serial_grab {
     my ($self, $name) = @_;
 
-    # Connect to VM host, or, in case of Hyper-V, to intermediary from which we gather
-    # remote serial console output.
-    my ($hostname, $password);
-    if (check_var('VIRSH_VMM_FAMILY', 'hyperv')) {
-        $hostname = get_required_var('VIRSH_GUEST');
-        $password = get_var('VIRSH_GUEST_PASSWORD');
-    }
-    else {
-        $hostname = get_required_var('VIRSH_HOSTNAME');
-        $password = get_var('VIRSH_PASSWORD');
-    }
     my $credentials = $self->read_credentials_from_virsh_variables;
     my $chan        = $self->start_ssh_serial(%$credentials);
     if (check_var('VIRSH_VMM_FAMILY', 'vmware')) {