pevik/os-autoinst

commit 9b618c7c852ec4a1fd7c1ffbe111efd221d8f795

Allow adding additional serial port when using svirt backend
See https://progress.opensuse.org/issues/36457

consoles/sshVirtsh.pm
@@ -68,6 +68,8 @@ sub activate {
     $self->_init_xml();
 }
 
+# creates an XML document to configure the libvirt domain
+# (see https://libvirt.org/formatdomain.html for the specification of that config file)
 sub _init_xml {
     my ($self, $args) = @_;
 
@@ -198,6 +200,7 @@ sub change_domain_element {
     return;
 }
 
+# adds the serial console used for the serial log
 sub add_pty {
     my ($self, $args) = @_;
 
@@ -256,6 +259,33 @@ sub add_vnc {
     $elem->setAttribute(address => '0.0.0.0');
     $graphics->appendChild($elem);
 
+    return;
+}
+
+# adds a further serial port
+# (in addition to the serial console on port 0 which added in add_pty, so don't use port 0 here)
+sub add_serial_console {
+    my ($self, $args) = @_;
+
+    my $doc     = $self->{domainxml};
+    my $devices = $self->{devices_element};
+    my $port    = $args->{port} // '1';
+
+    my $serial = $doc->createElement('serial');
+    $serial->setAttribute(type => 'pty');
+
+    # set the port number
+    my $target = $doc->createElement('target');
+    $target->setAttribute(port => $port);
+    $serial->appendChild($target);
+
+    # set the name of the serial port used to refer to it when calling 'virsh console'
+    # note: This doesn't seem to have any effect, but set it in accordance with the 'default' we expect to get.
+    my $alias = $doc->createElement('alias');
+    $alias->setAttribute(name => 'serial' . $port);
+    $serial->appendChild($alias);
+
+    $devices->appendChild($serial);
     return;
 }
 

commit 8f20ab96ec47d4fd90b38812b70365eed24a75b0

svirt: Fix serial backend for s390x
s390x needs to use <console> for another serial device instead of
<serial>. On the other hand it looks like x86 needs <serial>.
Therefore new parameter is pty_dev is added to consoles::sshVirtsh
and used in start_serial_grab().

Although this backend is probably not going to be used on x86 (plain
qemu backend with virtio console is better option), keep <serial> as
default. This backend is planned to be used on s390x.

+ reuse add_pty() in add_serial_console() as <alias> tag is not needed.

Fixes: 9b618c7c ("Allow adding additional serial port when using svirt backend")

backend/svirt.pm
@@ -1,5 +1,5 @@
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2016 SUSE LLC
+# Copyright © 2012-2019 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -272,7 +272,7 @@ sub start_serial_grab {
 
 # opens another SSH connection to grab the serial console with the specified port
 sub open_serial_console_via_ssh {
-    my ($self, $name, $port) = @_;
+    my ($self, $name, $devname, $port) = @_;
 
     bmwqemu::diag("Starting SSH connection to connect to libvirt domain $name via serial port $port");
     my $credentials = $self->read_credentials_from_virsh_variables;
@@ -293,7 +293,7 @@ sub open_serial_console_via_ssh {
         $chan->exec('nc ' . get_var('HYPERV_SERVER') . ' ' . $port);
     }
     else {
-        $chan->exec("virsh console \"$name\" \"serial$port\"");
+        $chan->exec("virsh console \"$name\" \"$devname$port\"");
     }
 
     return ($ssh, $chan);

consoles/sshVirtsh.pm
@@ -1,5 +1,5 @@
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2018 SUSE LLC
+# Copyright © 2012-2019 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -268,29 +268,13 @@ sub add_vnc {
 
 # adds a further serial port
 # (in addition to the serial console on port 0 which added in add_pty, so don't use port 0 here)
+# As it's used over virsh console, use <console>.
 sub add_serial_console {
     my ($self, $args) = @_;
 
-    my $doc     = $self->{domainxml};
-    my $devices = $self->{devices_element};
-    my $port    = $args->{port} // '1';
-
-    my $serial = $doc->createElement('serial');
-    $serial->setAttribute(type => 'pty');
-
-    # set the port number
-    my $target = $doc->createElement('target');
-    $target->setAttribute(port => $port);
-    $serial->appendChild($target);
-
-    # set the name of the serial port used to refer to it when calling 'virsh console'
-    # note: This doesn't seem to have any effect, but set it in accordance with the 'default' we expect to get.
-    my $alias = $doc->createElement('alias');
-    $alias->setAttribute(name => 'serial' . $port);
-    $serial->appendChild($alias);
-
-    $devices->appendChild($serial);
-    return;
+    my $port    = $args->{port}    // '1';
+    my $pty_dev = $args->{pty_dev} // 'console';
+    $self->add_pty({pty_dev => $pty_dev, pty_dev_type => 'pty', target_port => $port});
 }
 
 sub add_input {

consoles/sshVirtshSUT.pm
@@ -33,6 +33,14 @@ sub new {
     $self->{libvirt_domain} = $args->{libvirt_domain} // "openQA-SUT-$instance";
     $self->{serial_port_no} = $args->{serial_port_no} // 1;
 
+    # QEMU on s390x fails to start when added <serial> device due arch limitation
+    # on SCLP console, see "Multiple VT220 operator consoles are not supported"
+    # error at
+    # https://github.com/qemu/qemu/blob/master/hw/char/sclpconsole.c#L226
+    # Therefore <console> must be used for s390x.
+    # ATM there is only s390x using this console, let's make it the default.
+    $self->{pty_dev} = $args->{pty_dev} // 'console';
+
     return $self;
 }
 
@@ -58,7 +66,7 @@ sub activate {
     my ($self) = @_;
 
     my $backend = $self->{backend};
-    my ($ssh, $chan) = $backend->open_serial_console_via_ssh($self->{libvirt_domain}, $self->{serial_port_no});
+    my ($ssh, $chan) = $backend->open_serial_console_via_ssh($self->{libvirt_domain}, $self->{pty_dev}, $self->{serial_port_no});
 
     $self->{ssh}    = $ssh;
     $self->{screen} = consoles::virtio_screen->new($chan, $ssh->sock);

t/22-svirt.t
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+# Copyright © 2018-2019 SUSE LLC
 
 use strict;
 use warnings;
@@ -59,7 +60,7 @@ subtest 'XML config for VNC and serial console' => sub {
     $svirt_console->_init_xml();
     $svirt_console->add_vnc({port => 5901});
     $svirt_console->add_pty({target_port => 0});
-    $svirt_console->add_serial_console();
+    $svirt_console->add_serial_console({pty_dev => 'console', target_port => 1});
 
     my $produced_xml = $svirt_console->{domainxml}->toString(2);
     my $expected_xml = '22-svirth-virsh-config.xml';